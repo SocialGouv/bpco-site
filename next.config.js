da22716f15077b537e20ad9b8649b97506c22df7
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-03-08 00:05:48 +0100

    chore: add react-dsfr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.woff2$/,
+      type: "asset/resource"
+    });
+    return config;
+  },
+  //This option requires Next 13.1 or newer, if you can't update you can use this plugin instead: https://github.com/martpie/next-transpile-modules
+  transpilePackages: ["@codegouvfr/react-dsfr"]
 }
 
 module.exports = nextConfig
